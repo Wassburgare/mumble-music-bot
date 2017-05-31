3223a14476048a1b006ab09716fa7d03f1173931
Wassburgare <daniel.wassbjer@gmail.com>
2017-05-31 15:12:53 +0200

    Remove code that wasn't supposed to be pushed

--- src/MusicPlayer.js
+++ src/MusicPlayer.js
@@ -3,47 +3,22 @@ import fs from 'fs';
 import EventEmitter from 'events';
 import config from 'config';
 
-const getSongs = () => {
-  const musicDir = config.get('musicDir');
-  const regex = /\.(mp3)$/i;
-
-  return fs.readdirSync(musicDir)
-    .filter(song => regex.test(song))
-    .map(song => `${musicDir}/${song}`);
-};
-
-const getRandomSong = () => {
-  const songs = getSongs();
-
-  return songs[Math.floor(Math.random() * songs.length)];
-};
-
-const getSong = () => {
-  const playOrder = config.get('playOrder');
-  switch (playOrder) {
-    case 'random':
-      return getRandomSong();
-    case 'loop':
-      return getRandomSong();
-    default:
-      console.warn(`Invalid config value "${playOrder}" for playOrder specified. Playing random song instead.`);
-      return getRandomSong();
-  }
-};
-
 const nextSong = () => {
   if (!process.stdin.isTTY) {
     return process.stdin;
   }
 
-  const song = getSong();
+  const musicDir = config.get('musicDir');
+  // Should probably improve this file check...
+  const songs = fs.readdirSync(musicDir).filter(song => /\.(mp3)$/i.test(song));
+  const songName = songs[Math.floor(Math.random() * songs.length)];
 
-  if (!song) {
-    console.error(`No MP3 file was found in ${config.get('musicDir')}.`);
+  if (!songName) {
+    console.error(`No MP3 file was found in ${musicDir}.`);
     process.exit(1);
   }
 
-  return fs.createReadStream(song);
+  return fs.createReadStream(`${musicDir}/${songName}`);
 };
 
 class MusicPlayer extends EventEmitter {
