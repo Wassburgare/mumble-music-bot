cb884ecd54ac9ad7285950d93766646bec3afc76
Wassburgare <daniel.wassbjer@gmail.com>
2017-06-10 16:48:57 +0200

    Update option descriptions

--- src/program.js
+++ src/program.js
@@ -1,13 +1,13 @@
 import program from 'commander';
 
 program
-  .option('-H, --host [host]', 'Hostname of Mumble server')
-  .option('-p, --port [port]', 'Port of Mumble server')
-  .option('-n, --bot-name [name]', 'Username of bot')
-  .option('-m, --mute-users [users]', 'Bot mutes itself when users in this list are unmuted')
-  .option('-j, --join-users [users]', 'Bot mutes itself when users in this list are connected')
-  .option('-M, --music-dir [dir]', 'Path to music directory')
-  .option('-c, --config-dir [dir]', 'Set directory of config file', '')
+  .option('-H, --host [host]', 'Set hostname of Mumble server')
+  .option('-p, --port [port]', 'Set port of Mumble server')
+  .option('-n, --bot-name [name]', 'Set username of bot')
+  .option('-m, --mute-users [users]', 'Set list of users bot should mute itself on when users are unmuted')
+  .option('-j, --join-users [users]', 'Set list of users bot should mute itself on when users are connected')
+  .option('-M, --music-dir [dir]', 'Set path to music directory')
+  .option('-c, --config-dir [dir]', 'Set path to directory of config file', '')
   .parse(process.argv);
 
 export default program;
